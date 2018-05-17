bc5cd6a448ee25c2aebc7d2758bfebef6c57da93
Beverly Pham <beverly.pham@gmail.com>
2018-05-16 23:04:25 -0700

    make result component

--- src/SearchResultList/search-result.js
+++ src/SearchResultList/search-result.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
 export default class SearchResult extends Component {
+  constructor(props) {
+    super(props)
+  }
 
   render() {
     return (
       <div className="search-result">
-
+        <ul>
+          <li><a href="{this.props.results.topic.url}">
+            <h2>{this.props.results.topic.title}</h2>
+            <p>{this.props.results.topic.ups}</p>
+          </a></li>
+        </ul>
       </div>
     )
   }
