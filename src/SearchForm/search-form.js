f305d0b497796b5f46d22e70450b56c7704715aa
Beverly Pham <beverly.pham@gmail.com>
2018-05-16 22:33:09 -0700

    create-react-app scaffold

---
+++ src/SearchForm/search-form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class SearchForm extends Component {
+
+  render() {
+    return (
+      <div className="search-form">
+        <form action=""></form>
+      </div>
+    )
+  }
+}
+
